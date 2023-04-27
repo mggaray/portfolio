11eb0e5b7eb7b46c48e64a5bb3ff3afce59cd9ff
mggaray <maty1173@gmail.com>
2023-04-27 14:23:43 -0300

    projects links updated

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// hide unseen objects
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -10,3 +11,23 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+//blur other projects
+let projects = document.querySelectorAll(".projects__item__image");
+
+const blurProjects = (project) => {
+  let i = project.id;
+  projects.forEach((p) => {
+    if (i !== p.id) p.classList.toggle("blurred");
+  });
+};
+
+const unblur = () => {
+  projects.forEach((p) => p.classList.remove("blurred"));
+};
+
+projects.forEach((project, i) => {
+  project.setAttribute("id", i);
+  project.addEventListener("mouseover", () => blurProjects(project));
+  project.addEventListener("mouseout", () => unblur());
+});
